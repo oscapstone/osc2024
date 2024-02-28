996ef8b445cc8a4f4535f196d1eb6c6d1ebf0e68
alanasdw <alanasdw@gmail.com>
2024-02-28 09:39:38 +0000

    updating the dockerfile and pretty much not using env.sh make the dockerfile include the user permission for accessing the screen

--- lab0/env.sh
+++ lab0/env.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install build-essential git -y
-
-# install the cross-compiler
-sudo apt install gcc-aarch64-linux-gnu -y
-
-# install the qemu emulator for arm
-sudo apt install qemu-system-arm -y
-
-# install screen for connecting to rpi
-sudo apt install screen
+# adding the user to get the device in docker
+sudo adduser $USER dialout
