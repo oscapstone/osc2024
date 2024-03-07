553764589e42ead8e19cc118752d1f47b021e33a
eric <ericlin66.cs11@nycu.edu.tw>
2024-03-08 01:05:31 +0800

    [311554046] Lab1 Mini UART

---
+++ Lab1/include/peripheral/gpio.h
@@ -0,0 +1,11 @@
+#ifndef P_GPIO_H
+#define P_GPIO_H
+
+#include "peripheral/base.h"
+
+#define GPFSEL1 (PBASE + 0x00200004)
+
+#define GPPUD (PBASE + 0x00200094)
+#define GPPUDCLK0 (PBASE + 0x00200098)
+
+#endif /* P_GPIO_H */
--- Lab1/src/kernel.c
+++ Lab1/src/kernel.c
@@ -1,5 +1,11 @@
+#include "mini_uart.h"
+
 __attribute__((noreturn))
 void kernel_main(void)
 {
-    while (1);
+    uart_init();
+    uart_send_string("Hello, world!\r\n\0");
+
+    while (1)
+        uart_send(uart_recv());
 }
