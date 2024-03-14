ac2de1c4b911ea2d919dad3c39c4cfe02c7817bb
YouChengHan <kevin90728@gmail.com>
2024-03-14 20:15:33 +0800

    Finish lab1

---
+++ lab1/mbox.h
@@ -0,0 +1,24 @@
+extern volatile unsigned int mbox[36];
+
+#define MBOX_REQUEST    0
+
+/* channels */
+#define MBOX_CH_POWER   0
+#define MBOX_CH_FB      1
+#define MBOX_CH_VUART   2
+#define MBOX_CH_VCHIQ   3
+#define MBOX_CH_LEDS    4
+#define MBOX_CH_BTNS    5
+#define MBOX_CH_TOUCH   6
+#define MBOX_CH_COUNT   7
+#define MBOX_CH_PROP    8
+
+/* tags */
+#define MBOX_TAG_GETSERIAL      0x00010004
+#define MBOX_TAG_GETBRDREV      0x00010002
+#define MBOX_TAG_GETARMMEM	    0x00010005
+#define MBOX_TAG_LAST           0
+
+int mbox_call(unsigned char ch);
+int get_board_revision();
+int get_arm_memory();
