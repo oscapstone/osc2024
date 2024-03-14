53655cb5be804c77c1c30a608690c8e0c37fb8d3
Jay Lee <jayin920805@gmail.com>
2024-03-14 17:11:59 +0800

    lab1: fix several bugs

--- lab1/src/shell.c
+++ lab1/src/shell.c
@@ -2,6 +2,28 @@
 #include "mbox.h"
 #include "reboot.h"
 
+unsigned int is_visible(unsigned int c){
+    if(c >= 32 && c <= 126){
+        return 1;
+    }
+    return 0;
+}
+
+unsigned int strcmp(char* str1, char* str2){
+    int i = 0;
+    while(str1[i] != '\0' && str2[i] != '\0'){
+        if(str1[i] != str2[i]){
+            return 0;
+        }
+        i++;
+    }
+    if(str1[i] != '\0' || str2[i] != '\0'){
+        return 0;
+    }
+    return 1;
+}
+
+
 void uart_recv_command(char *str){
     char c;
     int i = 0;
@@ -19,31 +41,19 @@ void uart_recv_command(char *str){
             }
             continue;
         }
-        str[i] = c;
-        i++;
-        uart_send(c);
-    }
-
-}
-
-unsigned int strcmp(char* str1, char* str2){
-    int i = 0;
-    while(str1[i] != '\0' && str2[i] != '\0'){
-        if(str1[i] != str2[i]){
-            return 0;
+        if(is_visible(c)){
+            str[i] = c;
+            i++;
+            uart_send(c);
         }
-        i++;
     }
-    if(str1[i] != '\0' || str2[i] != '\0'){
-        return 0;
-    }
-    return 1;
+
 }
 
 void shell(){
     uart_send_string("Welcome to OSC2024 shell!\n");
     while(1){
-        uart_send_string("$ ");
+        uart_send_string("# ");
         char str[100];
         uart_recv_command(str);
         // uart_send_string(str);
@@ -57,16 +67,12 @@ void shell(){
         } else if (strcmp(str, "reboot")){
             uart_send_string("Rebooting...\n");
             reset(200);
+            break;
+        } else if(strcmp(str, "help")){
+            uart_send_string("help\t: print this help menu\n");
+            uart_send_string("hello\t: print Hello World!\n");
+            uart_send_string("mailbox\t: print board revision and memory info\n");
+            uart_send_string("reboot\t: reboot this device\n");
         }
-        else if(strcmp(str, "help")){
-            uart_send_string("help     : print this help menu\n");
-            uart_send_string("hello    : print Hello World!\n");
-            uart_send_string("mailbox  : print board revision and memory info\n");
-            uart_send_string("reboot   : reboot this device\n");
-        }
-      
-        // uart_send_string("\n");
-        // uart_send_string("You entered: ");
-        // uart_send_string(str);
     }
 }
