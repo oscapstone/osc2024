08fd0c0a85591f5fe5470bc24787f83c638489d8
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-03-14 14:40:08 +0800

    feat: improve panicking behavior

--- crates/panic-wait/src/lib.rs
+++ crates/panic-wait/src/lib.rs
@@ -3,8 +3,33 @@
 #![no_std]
 
 use core::panic::PanicInfo;
+use fmt::println;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    panic_prevent_reenter();
+
+    println!("{}", info);
+
+    cpu::wait_forever();
+}
+
+/// Stop immediately if called a second time.
+fn panic_prevent_reenter() {
+    // Using atoms can save us from needing `unsafe` here.
+    use core::sync::atomic::{AtomicBool, Ordering};
+
+    #[cfg(not(target_arch = "aarch64"))]
+    compile_error!(
+        "Add the target_arch to the check above if atomic bools' load/store are safe to use"
+    );
+
+    static PANIC_IN_PROGRESS: AtomicBool = AtomicBool::new(false);
+
+    if !PANIC_IN_PROGRESS.load(Ordering::Relaxed) {
+        PANIC_IN_PROGRESS.store(true, Ordering::Relaxed);
+        return;
+    }
+
     cpu::wait_forever();
 }
