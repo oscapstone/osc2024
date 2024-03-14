a0028e2f82b7c304615927dc8eb8858f8daf7da2
YJack0000 <jackleagends@gmail.com>
2024-03-14 18:50:21 +0800

    feat: add backspace support

--- lab1/kernel/console.c
+++ lab1/kernel/console.c
@@ -59,6 +59,15 @@ void _readcmd(char *x) {
   x[0] = 0;
   int input_index = 0;
   while ((input_char = read_c()) != '\n') {
+    if (input_char == '\b' || input_char == 127) { // 處理退格鍵
+      if (input_index > 0) { // 確保有字元可以刪除
+        input_index -= 1;    // 刪除最後一個字元
+        print_string(
+            "\b \b"); // 在控制台上刪除這個字元，\b
+                      // 是回退一格，空格刪除字元，再一個 \b 是將光標回退一格。
+        continue;
+      }
+    }
     x[input_index] = input_char;
     input_index += 1;
     print_char(input_char);
