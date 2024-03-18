826eeaeddc33f206ba62c1e495d58d6e2d65f873
Allen Guan <me@nella17.tw>
2024-03-19 02:58:58 +0800

    feat(lab2): print progress in upload.py

--- script/upload.py
+++ script/upload.py
@@ -8,10 +8,21 @@ with open("./disk/kernel.img", "rb") as f:
     size = len(kernel)
 
 with open(SERIAL, "wb", buffering=0) as tty:
+    print("reboot rpi")
+    tty.write(b"reboot\n")
+    time.sleep(2)
+
+    print("send command")
     tty.write(b"s")
-    time.sleep(1)
+    time.sleep(0.1)
+
+    print("send kernel size")
     tty.write(struct.pack("<I", size))
-    time.sleep(1)
+    time.sleep(0.1)
+
+    print("send kernel")
     tty.write(kernel)
     time.sleep(1)
+
+    print("jump kernel")
     tty.write(b"j")
