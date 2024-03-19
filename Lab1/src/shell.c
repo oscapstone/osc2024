ccd311bff09afa474bc66568c501d9b29c6c58be
eric <ericlin66.cs11@nycu.edu.tw>
2024-03-19 21:31:02 +0800

    Modify the comment of the cancel_reset function in src/shell.c

--- Lab1/src/shell.c
+++ Lab1/src/shell.c
@@ -53,7 +53,7 @@ void cancel_reset(void)
 {
     uart_send_string("reboot canceled\n");
     EXIT = 0;
-    put32(PM_RSTC, PM_PASSWORD | 0);  // full reset
+    put32(PM_RSTC, PM_PASSWORD | 0);  // cancel reset
     put32(PM_WDOG, PM_PASSWORD | 0);  // number of watchdog tick
 }
 
