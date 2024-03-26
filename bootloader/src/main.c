3b652cd45757c2ce9c3f28ec50f681393112049b
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2024-03-27 02:47:42 +0800

    bootloader start and linker

---
+++ bootloader/src/main.c
@@ -0,0 +1,31 @@
+#include "uart.h"
+
+void load_img() {
+  unsigned int size = 0;
+  unsigned char *size_buffer = (unsigned char *)&size;
+
+  for (int i = 0; i < 4; i++) {
+    size_buffer[i] = uart_read();
+  }
+  uart_printf("Image size: %x\n", size);
+
+  char *kernel = (char *)0x80000;
+  while (size--) {
+    *(kernel++) = uart_read();
+  }
+
+  uart_println("Image loaded");
+
+  /* In AArch64 state, the Link Register(LR) stores the return address when a
+   * subroutine call is made */
+  asm volatile("mov x30, 0x80000;");
+  asm volatile("ret");
+}
+
+void main() {
+  // set up serial console
+  uart_init();
+
+  uart_println("Start Bootloading...");
+  load_img();
+}
