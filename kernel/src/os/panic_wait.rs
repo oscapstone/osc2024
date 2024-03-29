8da735d87b5266df0fb7e26810166a7eb335e39a
zolark173 <clark.20011120@gmail.com>
2024-03-29 17:29:48 +0800

    Panic function will print error

--- kernel/src/os/panic_wait.rs
+++ kernel/src/os/panic_wait.rs
@@ -1,6 +1,10 @@
 use core::panic::PanicInfo;
+use alloc::string::ToString;
+
+use crate::os::stdio::*;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    unimplemented!()
+fn panic(info: &PanicInfo) -> ! {
+    println(info.to_string().as_str());
+    loop {}
 }
