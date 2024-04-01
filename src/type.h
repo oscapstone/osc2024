76ce4deda132991e787e072b8754c5c43813ac59
flyotlin <flyotlin@gmail.com>
2024-04-01 11:24:01 +0800

    feat(lab2): add std type for device tree

---
+++ src/type.h
@@ -0,0 +1,9 @@
+#ifndef _DEF_TYPE
+#define _DEF_TYPE
+
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned int    uint32_t;
+typedef unsigned long   uint64_t;
+
+#endif
