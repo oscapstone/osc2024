977e366836dcaee724b4ab18e976cc24e387d53b
zolark173 <clark.20011120@gmail.com>
2024-04-09 00:57:09 +0800

    Add exception to simple program

--- simple_program/src/main.rs
+++ simple_program/src/main.rs
@@ -1,12 +1,24 @@
 #![no_main]
 #![no_std]
 
+use core::arch::{asm, global_asm};
+
 mod cpu;
 mod os;
 extern crate alloc;
 
+global_asm!(include_str!("boot.s"));
+
 #[no_mangle]
 fn main() {
+    // let mut el: u64 = 0;
+    unsafe {
+        cpu::uart::initialize();
+        // asm!("mrs {tmp}, CurrentEL", tmp = out(reg) el);
+        // asm!("nop");
+    }
     println!("Hello, world!");
+    // println!("Current EL: {}", el >> 2);
+
     loop {}
 }
