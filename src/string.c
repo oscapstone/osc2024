a51ad7ca139915ccfbf4edfa6b8ce233844167c6
flyotlin <flyotlin@gmail.com>
2024-04-09 15:56:28 +0800

    fix(breaking change): use correct strlen_len() in strcmp()

--- src/string.c
+++ src/string.c
@@ -18,8 +18,8 @@ int strlen(const char* s)
 
 int strcmp(const char* s1, const char* s2)
 {
-    int len1 = strlen(s1);
-    int len2 = strlen(s2);
+    int len1 = strlen_new(s1);
+    int len2 = strlen_new(s2);
 
     if (len1 != len2) {
         return len1 - len2;
