50a5f9766998688de58545a20cbd2917731df6e0
Allen Guan <me@nella17.tw>
2024-04-10 21:03:06 +0800

    feat(lab3): print exception context

--- include/kernel/exception.hpp
+++ include/kernel/exception.hpp
@@ -2,10 +2,18 @@
 
 #include "util.hpp"
 
+struct ExceptionContext {
+  uint64_t X[30];
+  uint64_t lr;
+  uint64_t spsr_el1;
+  uint64_t elr_el1;
+  uint64_t esr_el1;
+};
+
 inline int get_el() {
   return read_sysreg(CurrentEL) >> 2;
 }
 
 extern "C" {
-void exception_entry();
+void exception_handler(ExceptionContext*, int type);
 }
--- src/kernel/exception.cpp
+++ src/kernel/exception.cpp
@@ -2,4 +2,9 @@
 
 #include "board/mini-uart.hpp"
 
-void exception_entry() {}
+void exception_handler(ExceptionContext* context, int type) {
+  mini_uart_printf("Exception Type: %d %d\n", type / 4, type % 4);
+  mini_uart_printf("SPSR_EL1: 0x%lx\n", context->spsr_el1);
+  mini_uart_printf("ELR_EL1: 0x%lx\n", context->elr_el1);
+  mini_uart_printf("ESR_EL1: 0x%lx\n", context->esr_el1);
+}
