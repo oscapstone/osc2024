1b06c3744a603deb41ed9f54d8eea9eb1310d319
Evian Chuang <evian.chuang@gmail.com>
2024-04-19 04:23:30 +0800

    Clean code

--- lab2/send_image_script/send_image_to_bootloader.py
+++ lab2/send_image_script/send_image_to_bootloader.py
@@ -6,9 +6,9 @@ import argparse
 from sys import platform
 
 if platform == "darwin":
-    parser = argparse.ArgumentParser(description='NYCU OSDI kernel sender')
+    parser = argparse.ArgumentParser(description='Kernel Sender')
     parser.add_argument('--filename', metavar='PATH', default='kernel8.img', type=str, help='path to kernel8.img')
-    parser.add_argument('--device', metavar='TTY',default='/dev/ttys024', type=str,  help='path to UART device')
+    parser.add_argument('--device', metavar='TTY',default='/dev/tty.usbserial-0001', type=str,  help='path to UART device')
     parser.add_argument('--baud', metavar='Hz',default=115200, type=int,  help='baud rate')
     args = parser.parse_args()
 
@@ -23,40 +23,13 @@ if platform == "darwin":
                 ser.write(p64(length)[i:i+1])
                 ser.flush()
 
-            print("Start sending kernel image by uart1...")
+            print("Start sending kernel by uart1...")
             for i in range(length):
-                # Use kernel_raw[i: i+1] is byte type. Instead of using kernel_raw[i] it will retrieve int type then cause error
+                # Use kernel_raw[i: i+1] is byte type. 
+                # Using kernel_raw[i] it will retrieve int type then cause error
                 ser.write(kernel_raw[i: i+1])
                 ser.flush()
                 if i % 100 == 0:
                     print("{:>6}/{:>6} bytes".format(i, length))
             print("{:>6}/{:>6} bytes".format(length, length))
-            print("Transfer finished!")
-
-else:
-    parser = argparse.ArgumentParser(description='NYCU OSDI kernel sender')
-    parser.add_argument('--filename', metavar='PATH', default='kernel8.img', type=str, help='path to kernel8.img')
-    parser.add_argument('--device', metavar='COM',default='COM3', type=str,  help='COM# to UART device')
-    parser.add_argument('--baud', metavar='Hz',default=115200, type=int,  help='baud rate')
-    args = parser.parse_args()
-
-    with open(args.filename,'rb') as fd:
-        with Serial(args.device, args.baud) as ser:
-
-            kernel_raw = fd.read()
-            length = len(kernel_raw)
-
-            print("Kernel image size : ", hex(length))
-            for i in range(8):
-                ser.write(p64(length)[i:i+1])
-                ser.flush()
-
-            print("Start sending kernel image by uart1...")
-            for i in range(length):
-                # Use kernel_raw[i: i+1] is byte type. Instead of using kernel_raw[i] it will retrieve int type then cause error
-                ser.write(kernel_raw[i: i+1])
-                ser.flush()
-                if i % 100 == 0:
-                    print("{:>6}/{:>6} bytes".format(i, length))
-            print("{:>6}/{:>6} bytes".format(length, length))
-            print("Transfer finished!")
+            print("Done!")
