1b40a67876772d7d47522822f17955ae0627d0f9
yui0303 <iampeter62@gmail.com>
2024-04-19 23:51:45 +0800

    init bootloader

---
+++ lab04/bootloader/include/mini_uart.h
@@ -0,0 +1,26 @@
+#ifndef _MINI_UART_H
+#define _MINI_UART_H
+
+#include "base.h"
+
+#define AUX_ENABLES     ((volatile unsigned int*)(MMIO_BASE+0x00215004))
+#define AUX_MU_IO_REG   ((volatile unsigned int*)(MMIO_BASE+0x00215040))
+#define AUX_MU_IER_REG  ((volatile unsigned int*)(MMIO_BASE+0x00215044))
+#define AUX_MU_IIR_REG  ((volatile unsigned int*)(MMIO_BASE+0x00215048))
+#define AUX_MU_LCR_REG  ((volatile unsigned int*)(MMIO_BASE+0x0021504C))
+#define AUX_MU_MCR_REG  ((volatile unsigned int*)(MMIO_BASE+0x00215050))
+#define AUX_MU_LSR_REG  ((volatile unsigned int*)(MMIO_BASE+0x00215054))
+#define AUX_MU_MSR_REG  ((volatile unsigned int*)(MMIO_BASE+0x00215058))
+#define AUX_MU_SCRATCH  ((volatile unsigned int*)(MMIO_BASE+0x0021505C))
+#define AUX_MU_CNTL_REG ((volatile unsigned int*)(MMIO_BASE+0x00215060))
+#define AUX_MU_STAT_REG ((volatile unsigned int*)(MMIO_BASE+0x00215064))
+#define AUX_MU_BAUD_REG ((volatile unsigned int*)(MMIO_BASE+0x00215068))
+
+
+void uart_init();
+char uart_recv();
+char uart_recv_io();
+void uart_send(char c);
+void uart_send_string(const char* str);
+
+#endif
