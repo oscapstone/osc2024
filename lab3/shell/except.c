ac9248ab99baea19b3e17ff192e78f2730860932
Hst0411 <73873452+Hst0411@users.noreply.github.com>
2024-04-22 00:40:09 +0800

    Update except.c

--- lab3/shell/except.c
+++ lab3/shell/except.c
@@ -53,18 +53,18 @@ uart_send_string("\n");
 */
 
 
-
+//timerlist
 void timer_irq_handler() {
 	//enable core_0_timer
 	unsigned int* address = (unsigned int*) CORE0_TIMER_IRQ_CTRL;
 	*address = 2;
 	
-	// Disable timer
+	// Disable timer interrup, 因為發生timer interrupt
 	asm volatile("msr cntp_ctl_el0,%0"::"r"(0));
 	// Disable interrupts to protect critical section
 	asm volatile("msr DAIFSet, 0xf");
 	
-	// timer's current count
+	// 讀取timer reg取得current timer
 	uint64_t current_time;
 	asm volatile("mrs %0, cntpct_el0":"=r"(current_time));
 
@@ -73,16 +73,13 @@ void timer_irq_handler() {
 
 		//Execute the callback
 		timer->callback(timer->data);
- 
+
 		// Remove timer from the list
 		timer_head = timer->next;
 		if (timer_head) {
 		    timer_head->prev = NULL;
 		}
-			
-		//free timer
-		
-		// Reprogram the hardware timer if there are still timers left
+		//如果timer是空或是新的
 		if(timer_head) {
 			asm volatile("msr cntp_cval_el0, %0"::"r"(timer_head->expiry));
 			asm volatile("msr cntp_ctl_el0,%0"::"r"(1));
@@ -90,12 +87,9 @@ void timer_irq_handler() {
 			// 沒timer就mask timer interrupt
 			asm volatile("msr cntp_ctl_el0,%0"::"r"(0));
 		}
-
-
 		//enable interrupt
 		asm volatile("msr DAIFClr,0xf");
 	}
-
 }
 
 //Basic3
@@ -182,37 +176,39 @@ void uart_receive_handler() {
 
 
 
+
+//timer, uart的read跟write都透過irq_except_handler()處理
+//每發生一個task就加入至tasklist
+//timer則是加在timer list裡
+//當時間觸發到,就轉移加入到tasklist
+//之後按照順序執行
+//basic3 advance2
 void irq_except_handler() {
     
     // Handle timeout interrupt
-    // 設定timeout後，當發生interrupt時，timeout interrupt handler處理interrupt
+    // 設定setTimeout後，當發生interrupt時，timeout interrupt handler處理interrupt
     asm volatile("msr DAIFSet, 0xf"); // Disable interrupts
-								  
-    uint32_t irq_pending1 = mmio_read(IRQ_PENDING_1);
+    //判斷是哪種interrupt
     uint32_t core0_interrupt_source = mmio_read(CORE0_INTERRUPT_SOURCE);
-    	
     uint32_t iir = mmio_read(AUX_MU_IIR);
     
     
-    
+    // Advance2
     // task interrupt
-    // CNTPSIRQ : Counter Physical Secure IRQ" status.
-    // 如果在secure mode下運作並且想要handle timer interrupt
-    // 使用CNTPSIRQ因為系統可能在non-secure mode
-    if (core0_interrupt_source & CNTPSIRQ_BIT_POSITION) {
-	
-	//disable core 0 timer
+    if (core0_interrupt_source) {
+	// 因為進入timer interrupt先關掉core0 timer
+	// 等處理一個階段再打開
+	// 避免中斷一半又有timer interrupt
 	unsigned int* address = (unsigned int*) CORE0_TIMER_IRQ_CTRL;
 	*address = 0;
-
 	create_task(timer_irq_handler,3);
     }
 
 
     // Basic3
-    // Handle UART interrupt
+    // Handle UART interrupt 
+    uint32_t irq_pending1 = mmio_read(IRQ_PENDING_1);
     if (irq_pending1 & AUXINIT_BIT_POSTION) {
-    
         // AUX_MU_IIR : interrupt status
         // 0x04 = 0x0100 -> receive
         // Check if it is a receive interrupt
@@ -224,7 +220,6 @@ void irq_except_handler() {
 	    mmio_write(AUX_MU_IER, mmio_read(AUX_MU_IER) & ~(0x01));
 	    create_task(uart_receive_handler, 1);
 	}
-	
 	// 0x02 = 0x0010 -> transmit
         // Check if it is a transmit interrupt
 	if ((iir & 0x06) == 0x02) {
@@ -232,14 +227,11 @@ void irq_except_handler() {
 	    // Disable transmit interrupt
 	    // 0x2 = 0x0010 -> enable transmit interrupt
 	    mmio_write(AUX_MU_IER, mmio_read(AUX_MU_IER) & ~(0x02));
-	    create_task(uart_transmit_handler,2);
+	    create_task(uart_transmit_handler, 2);
 	}
-    }	
-	
+    }
     asm volatile("msr DAIFClr, 0xf"); // Enable interrupts
-
     execute_tasks();
-	
 }
 
 
@@ -307,4 +299,3 @@ void uart_irq_handler(){
         }
     }	
 }
-
