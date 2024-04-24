e07f1df8fa6be3680351c08254bff4cf58aca444
flyotlin <flyotlin@gmail.com>
2024-04-25 02:11:44 +0800

    feat(lab3): disgusting timer on rpi3

--- kernel/timer.c
+++ kernel/timer.c
@@ -26,52 +26,80 @@ void add_timer(void)
         second = 10 * second + (line[i] - '0');
     }
 
-    // create timer_t
-    timer_t *timer = init_timer(message, second);
-    append_timer(timer);
+    messages[timer_index] = message;
+    seconds[timer_index] = second;
+    timer_index++;
+
+    // // create timer_t
+    // timer_t *timer = init_timer(message, second);
+    // uart_send('c');
+    // // uart_send('\n');
+    // append_timer(timer);
 }
 
-timer_t *init_timer(char *msg, int second)
-{
-    timer_t *timer = (timer_t *) malloc(sizeof(timer_t));
+// timer_t *init_timer(char *msg, int second)
+// {
+//     // uart_send('d');
+//     // timer_t *timer = (timer_t *) malloc(sizeof(timer_t));
+//     // uart_send('e');
+//     // uart_hex(timer);
+//     // timer->second = second;
+//     // uart_send('f');
+//     // timer->prev = NULL;
+//     // uart_send('g');
+//     // timer->next = NULL;
+//     // uart_send('h');
+//     // timer->message = msg;   // THIS breaks
+//     // uart_send('i');
 
-    timer->message = msg;
-    timer->second = second;
-    timer->prev = NULL;
-    timer->next = NULL;
+//     // return timer;
+// }
 
-    return timer;
-}
+// void append_timer(timer_t *timer)
+// {
+//     // uart_send('j');
+//     // if (timer_head == NULL) {
+//     //     uart_send('k');
+//     //     timer_head = timer;
+//     //     return;
+//     // }
 
-void append_timer(timer_t *timer)
-{
-    if (timer_head == NULL) {
-        timer_head = timer;
-        return;
-    }
-
-    timer_t *cur = timer_head;
-    while (cur->next != NULL) {
-        cur = cur->next;
-    }
-    cur->next = timer;
-    timer->prev = cur;
-}
+//     // uart_send('l');
+//     // timer_t *cur = timer_head;
+//     // while (cur->next != NULL) {
+//     //     cur = cur->next;
+//     //     uart_send('m');
+//     // }
+//     // uart_send('n');
+//     // // uart_send('\n');
+//     // cur->next = timer;
+//     // uart_send('o');
+//     // timer->prev = cur;
+//     // uart_send('p');
+// }
 
 void update_timers(void)
 {
-    timer_t *cur = timer_head;
-    if (cur == NULL) {
-        return;
-    }
-
-    while (cur != NULL) {
-        cur->second--;
-        if (cur->second == 0) {
-            uart_puts(cur->message);
-
-            // TODO: remove this one
+    for (int i = 0; i < timer_index; i++) {
+        if (seconds[i] >= 0) {
+            seconds[i]--;
         }
-        cur = cur->next;
+        if (seconds[i] == 0) {
+            uart_puts(messages[i]);
+        }
     }
+    // timer_t *cur = timer_head;
+    // if (cur == NULL) {
+    //     return;
+    // }
+
+    // while (cur != NULL) {
+    //     cur->second--;
+    //     if (cur->second == 0) {
+    //         uart_puts(cur->message);
+
+    //         // TODO: remove this one
+    //     }
+    //     cur = cur->next;
+    // }
 }
--- kernel/timer.h
+++ kernel/timer.h
@@ -3,18 +3,21 @@
 
 #include "malloc.h"
 
-typedef struct timer {
-    char *message;
-    int second;
-    struct timer *prev;
-    struct timer *next;
-} timer_t;
+// typedef struct timer {
+//     char *message;
+//     int second;
+//     struct timer *prev;
+//     struct timer *next;
+// } timer_t;
 
 void add_timer(void);
-timer_t *init_timer(char *msg, int second);
-void append_timer(timer_t *timer);
+// timer_t *init_timer(char *msg, int second);
+// void append_timer(timer_t *timer);
 void update_timers(void);
 
-static timer_t *timer_head = NULL;
+// static timer_t *timer_head = NULL;
+static char *messages[100];
+static int seconds[100];
+static int timer_index = 0;
 
 #endif
