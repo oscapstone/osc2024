22b0692d5fbc0a05e7a020cab3dcab7878e54df1
zolark173 <clark.20011120@gmail.com>
2024-04-25 23:53:13 +0800

    Fix CS_Counter problem

--- kernel/src/os/critical_section.rs
+++ kernel/src/os/critical_section.rs
@@ -1,20 +1,23 @@
-
 use core::arch::asm;
+use core::ptr::{read_volatile, write_volatile};
 
+const CS_COUNTER: *mut u32 = 0x7_5000 as *mut u32;
 
 pub fn disable_irq() {
     unsafe {
-        let mut counter: u32 = *(0x75000 as *mut u32);
+        let mut counter: u32 = read_volatile(CS_COUNTER);
         counter += 1;
         asm!("msr DAIFSet, 0xf");
+        write_volatile(CS_COUNTER, counter);
     }
 }
 pub fn enable_irq() {
     unsafe {
-        let mut counter: u32 = *(0x75000 as *mut u32);
+        let mut counter: u32 = read_volatile(CS_COUNTER);
         counter -= 1;
         if counter == 1 {
             asm!("msr DAIFClr, 0xf");
         }
+        write_volatile(CS_COUNTER, counter);
     }
 }
