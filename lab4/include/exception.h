eebcf874a71062b1e12ec30a091ee5f9a4749238
Jay Lee <jayin920805@gmail.com>
2024-05-11 13:50:23 +0800

    lab4: init

---
+++ lab4/include/exception.h
@@ -0,0 +1,21 @@
+#ifndef _EXECPTION_H_
+#define _EXECPTION_H_
+
+#define CORE0_INTERRUPT_SOURCE 0x40000060
+
+#define INTERRUPT_SOURCE_CNTPNSIRQ (1<<1)
+#define INTERRUPT_SOURCE_GPU (1<<8)
+
+#define IRQ_PENDING_1_AUX_INT (1<<29)
+
+void el1_interrupt_enable();
+void el1_interrupt_disable();
+
+void exception_handler_c();
+void irq_exception_handler_c();
+
+void irq_timer_exception();
+void irq_uart_rx_exception();
+void irq_uart_tx_exception();
+
+#endif
