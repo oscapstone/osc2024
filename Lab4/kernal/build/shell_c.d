c4335c935cdceeec1b3fd638ecf874322fc27313
poro0542 <jsf200111@gmail.com>
2024-05-30 16:45:48 +0800

    Lab4

--- Lab4/kernal/build/shell_c.d
+++
@@ -1,4 +0,0 @@
-build/shell_c.o: src/shell.c include/mini_uart.h include/stdio.h \
- include/shell.h include/str.h include/mailbox.h include/reboot.h \
- include/dtb.h include/cpio.h include/memalloc.h \
- include/peripherals/mini_uart.h include/peripherals/base.h include/irq.h
