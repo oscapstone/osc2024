96fa262504ef6045dd131e4edb06af986e447278
Allen Guan <me@nella17.tw>
2024-06-03 16:17:13 +0800

    refactor: string size_t

--- include/kernel/string.hpp
+++ include/kernel/string.hpp
@@ -8,13 +8,15 @@ char* strdup(const char* s);
 
 class string_view {
   const char* buf_;
-  long size_;
+  size_t size_;
 
  public:
   using iterator = const char*;
   string_view() : buf_(nullptr), size_(0) {}
   string_view(const char* buf) : buf_(buf), size_(strlen(buf)) {}
-  string_view(const char* buf, long size) : buf_(buf), size_(size) {}
+  template <typename T>
+    requires std::is_convertible_v<T, size_t>
+  string_view(const char* buf, T size) : buf_(buf), size_(size) {}
   iterator begin() {
     return buf_;
   }
@@ -24,17 +26,17 @@ class string_view {
   const char* data() {
     return buf_;
   }
-  long size() const {
+  size_t size() const {
     return size_;
   }
   bool empty() const {
     return size_ == 0;
   }
-  char operator[](long i) const {
+  char operator[](size_t i) const {
     return buf_[i];
   }
   bool printable() const {
-    for (long i = 0; i < size_; i++) {
+    for (size_t i = 0; i < size_; i++) {
       auto c = buf_[i];
       if (not(0x20 <= c and c <= 0x7e) and not(i + 1 == size_ and c == 0))
         return false;
@@ -49,10 +51,12 @@ class string {
 
  public:
   string() : beg_{nullptr}, end_{nullptr}, cap_{nullptr} {}
-  string(uint32_t cap) : string{} {
+  string(size_t cap) : string{} {
     reserve(cap);
   }
-  string(const char* s, uint32_t n) : string{} {
+  template <typename T>
+    requires std::is_convertible_v<T, size_t>
+  string(const char* s, T n) : string{} {
     append(s, n);
   }
   string(const char* s) : string(s, strlen(s)) {}
@@ -68,10 +72,10 @@ class string {
     return {data(), size()};
   }
 
-  void reserve(uint32_t new_cap) {
-    if (cap_ - beg_ < new_cap) {
+  void reserve(size_t new_cap) {
+    if (data() == nullptr or cap_ < beg_ + new_cap) {
       auto nstr = new char[new_cap + 1];
-      uint32_t n = size();
+      size_t n = size();
       memcpy(nstr, beg_, n);
       beg_ = nstr;
       end_ = nstr + n;
@@ -82,10 +86,10 @@ class string {
   char* data() const {
     return beg_;
   }
-  uint32_t size() const {
+  size_t size() const {
     return end_ - beg_;
   }
-  uint32_t cap() const {
+  size_t cap() const {
     return cap_ - beg_;
   }
 
@@ -101,11 +105,11 @@ class string {
     end_ += n;
     return *this;
   }
-  string& operator+=(uint32_t size) {
+  string& operator+=(size_t size) {
     end_ += size;
     return *this;
   }
-  char& operator[](uint32_t i) const {
+  char& operator[](size_t i) const {
     return data()[i];
   }
 };
