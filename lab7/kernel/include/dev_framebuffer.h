654d9910b4b51acc79db077d7f0d764c68d9eb05
FallMaple <tony53517230@gmail.com>
2024-06-13 17:17:06 +0800

    feat: add dev_framebuffer

---
+++ lab7/kernel/include/dev_framebuffer.h
@@ -0,0 +1,25 @@
+#ifndef _DEV_FRAMEBUFFER_H_
+#define _DEV_FRAMEBUFFER_H_
+
+#include "stddef.h"
+#include "vfs.h"
+#include "stdint.h"
+
+struct framebuffer_info
+{
+    unsigned int width;
+    unsigned int height;
+    unsigned int pitch;
+    unsigned int isrgb;
+};
+
+int init_dev_framebuffer();
+
+int  dev_framebuffer_write(struct file *file, const void *buf, size_t len);
+int  dev_framebuffer_read(struct file *file, void *buf, size_t len);
+int  dev_framebuffer_open(struct vnode *file_node, struct file **target);
+int  dev_framebuffer_close(struct file *file);
+int64_t dev_framebuffer_lseek64(struct file *file, int64_t offset, int whence);
+int  dev_framebuffer_op_deny();
+
+#endif
