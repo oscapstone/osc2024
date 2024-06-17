1e083b85af882871a6383b6e4d5cca66a5dc0fdd
kjw5656 <jiahyang10410@gmail.com>
2024-06-17 15:26:33 +0000

    Fix : clean the binary file

--- lab5/new_kernel/build/cpio_c.d
+++
@@ -1,2 +0,0 @@
-build/cpio_c.o: src/cpio.c include/cpio.h include/shell.h \
- include/utility.h
--- lab5/new_kernel/build/timer_c.d
+++
@@ -1,3 +0,0 @@
-build/timer_c.o: src/timer.c include/timer.h include/rpi_base.h \
- include/utility.h include/stdint.h include/mini_uart.h include/utility.h \
- include/memory.h
